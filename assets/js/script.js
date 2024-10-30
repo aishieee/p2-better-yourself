04f35cb39b38e3b1415865ae58be08148723e005
Aisha Sheikh <aishie_hk@icloud.com>
2024-10-30 14:29:05 +0000

    Set up event listener for habit form submission

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+// Select elements from the DOM
+const habitForm = document.getElementById('add-habit-form');
+const habitInput = document.getElementById('habit-input');
+const habitList = document.getElementById('habits');
