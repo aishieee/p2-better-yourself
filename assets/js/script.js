4fba25b709b92bdf538e4e59498c10c1224f6a23
Aisha Sheikh <aishie_hk@icloud.com>
2024-11-24 21:47:26 +0000

    Implement addHabit function structure

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,8 @@
 const habitForm = document.getElementById('add-habit-form');
 const habitInput = document.getElementById('habit-input');
 const habitList = document.getElementById('habits');
+
+// Function for user to add habit to the list from input field
+function addHabit(event) {
+    event.preventDefault();
+}
