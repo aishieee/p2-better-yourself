2596101483b358002990d0debf123db76703c5b6
Aisha Sheikh <aishie_hk@icloud.com>
2024-11-27 23:53:05 +0000

    Add function to submit new habits and log to console

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,9 @@ const habitSubmit = document.getElementById('habit-submit');
 const resetHabits = document.getElementById('reset-habits');
 const habitRows = document.getElementById('habit-rows');
 
-// Save habits to local storage
-function saveHabits(habits) {
-    localStorage.setItem('habits', JSON.stringify(habits));
-}
+// Function for user to add habit to the list from input field
+habitSubmit.addEventListener('click', function (event) {
+    event.preventDefault();
+    const habitName = habitInput.value.trim();
+    console.log('Habit added:', habitName);  // Log habit name to console for now
+});
