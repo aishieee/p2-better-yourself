271b7d90cd55567b77e2eab1c3c8637cf73f131d
Aisha Sheikh <aishie_hk@icloud.com>
2024-11-28 11:07:12 +0000

    Set up event listener for the 'Add habit' button

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,9 +4,9 @@ const habitSubmit = document.getElementById('habit-submit');
 const resetHabits = document.getElementById('reset-habits');
 const habitRows = document.getElementById('habit-rows');
 
-// Function for user to add habit to the list from input field
+// Function for user to use "Add habit" button 
 habitSubmit.addEventListener('click', function (event) {
     event.preventDefault();
     const habitName = habitInput.value.trim();
-    console.log('Habit added:', habitName);  // Log habit name to console for now
+    console.log('Habit added:', habitName);  // For now, log habit name to the console
 });
